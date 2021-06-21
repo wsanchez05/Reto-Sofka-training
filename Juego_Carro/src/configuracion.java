233377e47462a63a98d658e5f4faa46f466c4e66
William <William@hotmail.com>
2021-06-21 13:54:27 -0500

    ya se tiene realizado el programa hasta el podio

--- Juego_Carro/src/configuracion.java
+++ Juego_Carro/src/configuracion.java
@@ -48,16 +48,19 @@ public class configuracion
 	
 	public void info()
 	{
+// Se declaran variables
 		Scanner teclado = new Scanner(System.in);
 		int x;
 		System.out.println("Por favor ingrese el numero de corredores\n");
 		x= teclado.nextInt();
 		System.out.println("Por favor la distancia en kilomentros \n");
-		km = teclado.nextInt();
+		km = teclado.nextInt()*1000;
 		Cantidad_jugadores =x;
 		String [][] corredores = new String [x][2];
 		List<Double> Carriles = new ArrayList<Double>();
 		List<Double> Carros = new ArrayList<Double>();
+		
+	//Se piden los datos de informacion para el juego
 		for (int i=0; i<x;i++)
 		{
 			int n= i+1;
@@ -90,7 +93,8 @@ public class configuracion
 					Carros.add(N_carro);
 					
 		}
-		
+
+
 		for(int i=0; i<x; i++) 
 		{
 			int n= i+1;
@@ -104,45 +108,79 @@ public class configuracion
 		}
 	public void iniciar_juego ()
 	{
-		List<Double> Km_jugador = new ArrayList<Double>();
+		double [] Km_recorridos = new double [Cantidad_jugadores];
+		//List<Double> Km_jugador = new ArrayList<Double>();
 		System.out.println("Vamos a comenzar la carrera!! " );
-		double distancia_jugador;
+		double distancia_jugador=0.0;
 		String letra;
 		Scanner tecla =new Scanner(System.in);
 		double Tecla_dado=0;
+		double min=0;
+		String p_puesto;
+		String s_puesto;
+		String t_puesto;
 		
+// inicializar los datos de kilomentros recorridos de todos los jugadores en 0.		
 		for(int i=0; i<Cantidad_jugadores; i++) 
 		{
-			Km_jugador.add(i,0.0);
-			
+			//Km_jugador.add(i,0.0);
+			Km_recorridos[i]=0;
 		}
-		for(int i=0; i<Cantidad_jugadores; i++) 
-		{
-			int n= i+1;
-			
-			System.out.println("Por favor lanzar dado jugador con ID N° " +n + " presionando la tecla l ");
-			
-			
-		    letra = tecla.next();
-			if(letra.equals("l"))
-			{
-				Tecla_dado = new Random().nextInt(6)+1;
-				distancia_jugador = (Tecla_dado * 100) + Km_jugador.get(i);
-				
-				Km_jugador.add(i,distancia_jugador);
-			}
-			else 
-			{
-				System.out.println("Debes ingresar la tecla n, pierdes el avance.");
 
-			}
-			System.out.println("Sacaste el numero: " + Tecla_dado);	
-			System.out.println("La distancia recorrida del jugador N: " + n + " es: \n " +Km_jugador.get(i));	
-						
+		
+		//for (int k=0; k<Km_jugador.size();k++)
+		
+					
+		while(min<km)
+			
+		{
+			
+				for(int i=0; i<Cantidad_jugadores; i++) 
+				{
+					int n= i+1;
+					
+					System.out.println("Por favor lanzar dado jugador con ID N° " +n + " presionando la tecla l ");
+					
+					
+				    letra = tecla.next();
+					if(letra.equals("l"))
+					{
+						Tecla_dado = new Random().nextInt(6)+1;
+						//distancia_jugador = (Tecla_dado * 100) + Km_jugador.get(i);
+						distancia_jugador = (Tecla_dado * 100) + Km_recorridos[i];
+						//Km_jugador.add(i,distancia_jugador);
+						Km_recorridos[i]=distancia_jugador;
+					}
+					else 
+					{
+						System.out.println("Debes ingresar la tecla n, pierdes el avance.");
+		
+					}
+					System.out.println("Sacaste el numero: " + Tecla_dado);	
+					//System.out.println("La distancia recorrida del jugador N: " + n + " es: \n " +Km_jugador.get(i));	
+					System.out.println("La distancia recorrida del jugador N: " + n + " es: \n " +Km_recorridos[i]);		
+					
+					//if(Km_jugador.get(i)>=km)
+					if(Km_recorridos[i]>=km)
+					{
+						System.out.println("El jugador con ID: " + n + " ha terminado");
+					}
+					
+// se busca el jugador con el recorrido mas corto para que cuando se cumpla el limite se acabe el juego
+					
+						//if(Km_jugador.get(k)<min)
+						if(Km_recorridos[i]<min)
+							//min=Km_jugador.get(k);
+							min=Km_recorridos[i];
+						System.out.println(Km_recorridos[i]);
+				}
+				//System.out.println(Km_jugador);	
+				System.out.println(min);
+				System.out.println(km);
 		}
-				
 		
-		
+			
+	
 	}
 
 	}
